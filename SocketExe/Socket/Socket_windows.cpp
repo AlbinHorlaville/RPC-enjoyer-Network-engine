641bc65dadc8f215304769a26916e7ad9e6dd893
Albin Horlaville <albin.horlaville@etu.univ-grenoble-alpes.fr>
2025-01-28 12:33:18 -0500

    Fix: includes

--- SocketExe/Socket/Socket_windows.cpp
+++ SocketExe/Socket/Socket_windows.cpp
@@ -14,11 +14,11 @@ void Socket::initialize() {
 }
 
 ssize_t Socket::sendTo(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen) {
-    return ::sendto(sockfd, (const char*)buf, (int)len, flags, dest_addr, (int)addrlen);
+    return sendto(sockfd, (const char*)buf, (int)len, flags, dest_addr, (int)addrlen);
 }
 
 ssize_t Socket::recvFrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen) {
-    int received = ::recvfrom(sockfd, (char *)buf, (int)len, flags, src_addr, (int *)addrlen);
+    int received = recvfrom(sockfd, (char *)buf, (int)len, flags, src_addr, (int *)addrlen);
     if (received == SOCKET_ERROR) {
         int error_code = WSAGetLastError();
         std::cerr << "recvfrom failed. Error code: " << error_code << std::endl;
