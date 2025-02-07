dd549c0eb534e9accfceee90f05e3f8f0d81e294
Albin Horlaville <albin.horlaville@etu.univ-grenoble-alpes.fr>
2025-02-07 13:15:10 -0500

    Fix: Windows platform

--- SocketExe/Socket/Socket_windows.cpp
+++ SocketExe/Socket/Socket_windows.cpp
@@ -5,6 +5,52 @@
 #include "Socket.h"
 #include <iostream>
 
+std::string Socket::IpToString(const sockaddr* sa)
+{
+    switch(sa->sa_family)
+    {
+        case AF_INET: {
+            char ip[INET_ADDRSTRLEN + 6];
+            const char* ret = inet_ntop(AF_INET,
+                &reinterpret_cast<const sockaddr_in*>(sa)->sin_addr,
+                ip,
+                INET_ADDRSTRLEN);
+            return fmt::format("{}:{}", ret, ntohs(reinterpret_cast<const sockaddr_in*>(sa)->sin_port));
+        }
+        case AF_INET6: {
+            char ip[INET6_ADDRSTRLEN + 8];
+            const char* ret = inet_ntop(AF_INET6,
+                &reinterpret_cast<const sockaddr_in6*>(sa)->sin6_addr,
+                ip+ 1,
+                INET6_ADDRSTRLEN);
+            return fmt::format("[{}]:{}", ret, ntohs(reinterpret_cast<const sockaddr_in6*>(sa)->sin6_port));
+        }
+    }
+
+    return "";
+}
+
+sockaddr Socket::StringToIp(const std::string& ip, uint16_t port)
+{
+    sockaddr result {};
+    int error = inet_pton(AF_INET, ip.c_str(), &result);
+    if (error == 1) {
+        result.sa_family = AF_INET;
+        reinterpret_cast<sockaddr_in*>(&result)->sin_port = htons(port);
+        return result;
+    }
+
+    memset(&result, 0, sizeof(result));
+    error = inet_pton(AF_INET6, ip.c_str(), &result);
+    if (error == 1) {
+        result.sa_family = AF_INET6;
+        reinterpret_cast<sockaddr_in6*>(&result)->sin6_port = htons(port);
+        return result;
+    }
+    memset(&result, 0, sizeof(result));
+    return result;
+}
+
 void Socket::initialize() {
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
@@ -15,7 +61,7 @@ void Socket::initialize() {
 
 long Socket::sendTo(int sockfd, const std::string &to, uint16_t port, std::span<const char> message) {
     const sockaddr destination = StringToIp(to, port);
-    ssize_t error = sendto(sockfd,
+    long error = sendto(sockfd,
         message.data(),
         message.size(),
         0,
@@ -27,7 +73,7 @@ long Socket::sendTo(int sockfd, const std::string &to, uint16_t port, std::span<
 long Socket::recvFrom(int sockfd, std::string &from, std::span<char, 65535> message) {
     struct sockaddr_storage peer_addr;
     socklen_t peer_addr_len = sizeof(struct sockaddr_storage);
-    const ssize_t read_bytes = recvfrom(sockfd,
+    const long read_bytes = recvfrom(sockfd,
         message.data(),
         message.size_bytes(),
         0,
