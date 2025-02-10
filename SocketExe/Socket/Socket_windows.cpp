b8cf34257c9edbedfdf8cc9d66fb3bf4eedbd17f
Hugo Girard <127935820+Hugo0207@users.noreply.github.com>
2025-02-10 13:44:02 -0500

    Fix : removal of platform-specific StringToIp and IpToString

--- SocketExe/Socket/Socket_windows.cpp
+++ SocketExe/Socket/Socket_windows.cpp
@@ -5,52 +5,6 @@
 #include "Socket.h"
 #include <iostream>
 
-std::string Socket::IpToString(const sockaddr* sa)
-{
-    switch(sa->sa_family)
-    {
-        case AF_INET: {
-            char ip[INET_ADDRSTRLEN + 6];
-            const char* ret = inet_ntop(AF_INET,
-                &reinterpret_cast<const sockaddr_in*>(sa)->sin_addr,
-                ip,
-                INET_ADDRSTRLEN);
-            return fmt::format("{}:{}", ret, ntohs(reinterpret_cast<const sockaddr_in*>(sa)->sin_port));
-        }
-        case AF_INET6: {
-            char ip[INET6_ADDRSTRLEN + 8];
-            const char* ret = inet_ntop(AF_INET6,
-                &reinterpret_cast<const sockaddr_in6*>(sa)->sin6_addr,
-                ip+ 1,
-                INET6_ADDRSTRLEN);
-            return fmt::format("[{}]:{}", ret, ntohs(reinterpret_cast<const sockaddr_in6*>(sa)->sin6_port));
-        }
-    }
-
-    return "";
-}
-
-sockaddr Socket::StringToIp(const std::string& ip, uint16_t port)
-{
-    sockaddr result {};
-    int error = inet_pton(AF_INET, ip.c_str(), &result);
-    if (error == 1) {
-        result.sa_family = AF_INET;
-        reinterpret_cast<sockaddr_in*>(&result)->sin_port = htons(port);
-        return result;
-    }
-
-    memset(&result, 0, sizeof(result));
-    error = inet_pton(AF_INET6, ip.c_str(), &result);
-    if (error == 1) {
-        result.sa_family = AF_INET6;
-        reinterpret_cast<sockaddr_in6*>(&result)->sin6_port = htons(port);
-        return result;
-    }
-    memset(&result, 0, sizeof(result));
-    return result;
-}
-
 void Socket::initialize() {
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
